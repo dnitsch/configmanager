ff7472096f77e596cd2d452bff8454f030d3eb66
dnitsch <nitschneiderd@gmail.com>
2024-03-20 17:16:03 +0000

    fix: add errors to central place in package

--- pkg/generator/defaultstrategy.go
+++ pkg/generator/defaultstrategy.go
@@ -1,11 +1,19 @@
 package generator
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type DefaultStrategy struct {
 }
 
 const implementationNetworkErr string = "implementation %s error: %v for token: %s"
+
+var (
+	ErrEmptyResponse     = errors.New("value retrieved but empty for token")
+	ErrServiceCallFailed = errors.New("failed to complete the service call")
+)
 
 func NewDefatultStrategy() *DefaultStrategy {
 	return &DefaultStrategy{}
